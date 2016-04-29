f27e1bf9cc6523fa1d581e41592e008e2761c778
eric.metcalf <eric.metcalf@dealertrack.com>
2016-04-29 16:18:37 -0700

    Cleanup unused code.

--- client/utils/coreApiUtil.js
+++ client/utils/coreApiUtil.js
@@ -6,19 +6,6 @@ var ActionTypes = require('../constants/ActionTypes');
 var appUrl = config.services.api.protocol + config.services.api.host;
 
 var api = {
-    setPosition: {
-        retrieve: function(params, callback) {
-            request
-                .post(appUrl + '/set')
-                .send(params)
-                .end(function(error, res) {
-                    Dispatcher.dispatch({
-                        type: ActionTypes.actionTypes.SET_BOARD,
-                        payload: res.board 
-                    });
-                });
-        }
-    },
     startGame: {
         retrieve: function(callback) {
             request
