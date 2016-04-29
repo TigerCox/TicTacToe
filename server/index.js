999892cdbcab0b75c00244eaa0cfa41fcd333dba
JoJo <joseph.salomone@dealertrack.com>
2016-04-29 15:23:39 -0700

    Combined webpack and hapi into one server

--- server/index.js
+++ server/index.js
@@ -1,29 +1,22 @@
 var Hapi = require('hapi');
+var WebpackPlugin  = require('hapi-webpack-plugin');
 var routes = require('./routes');
 var Config = require('./config');
 var server = new Hapi.Server();
 
 server.settings.maxSockets = 300;
-
-//https unauth disable
-//HACK ALERT - Setting to allow widget to load - maybe
-//process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-
-//Ignore cookie parsing errors until setCookie, getCookie properly
-//encode and decode cookies in .NET solution
-//TODO: Remove when bug if fixed
 server.settings.connections.routes.state.failAction = 'ignore';
-
-//Make connection
 server.connection(Config.connection);
-
 server.route(routes);
 
-//Only start server if being called directly.
-if (!module.parent) {
-  server.start(function() {
-      console.log('Server started at ' + server.info.uri);
-  });
-}
+server.register({
+	register: WebpackPlugin,
+	options: './webpack.config.js'
+}, function(error) {
+	if (error) {
+		return console.error(error);
+	}
+	server.start(function() { console.log('Server running at:', server.info.uri) });
+});
 
 module.exports = server;
